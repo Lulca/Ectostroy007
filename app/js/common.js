6435c1446ed394a942cd2a285495370f8840c4ec
Lulca <Anatolkruv@gmail.com>
2016-10-20 23:55:24 +0300

    finish adaptive on 320px screen

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,36 @@
-$(function(){
-	
+
+var siteNavigation = (function() {
+
+	return {
+
+		init: function() {
+			var menu = $('.drop-down-nav');
+			var button = $('.icon-menu');
+			// var menuWrap = $('.menu-wrap');
+
+			button.on('click', function() {
+				$that = $(this);
+				var menu = $that.parent().parent().find('.drop-down-nav');
+				menu.fadeToggle();
+				button.addClass('icon-menu-dark');
+			});
+
+			$(window).resize(function(){
+				if ($(window).width() >= 992) {
+					menu.fadeIn(.1);
+				}
+
+				if ($(window).width() < 992) {
+					menu.fadeOut(.1);
+				}
+
+			});
+			
+		}
+	}
+})();
+
+
+$(document).ready(function() {
+	siteNavigation.init();
 });
