3d65b2ee310832e8a0395dbfd7e3c6ca281ed8cf
Lulca <Anatolkruv@gmail.com>
2016-10-21 03:05:19 +0300

    create drop-down nav in cities-tab section

--- app/js/common.js
+++ app/js/common.js
@@ -17,7 +17,7 @@ var siteNavigation = (function() {
 
 			$(window).resize(function(){
 				if ($(window).width() >= 992) {
-					menu.fadeIn(.1);
+					menu.fadeOut(.1);
 				}
 
 				if ($(window).width() < 992) {
@@ -30,7 +30,31 @@ var siteNavigation = (function() {
 	}
 })();
 
+var choseCity = (function() {
+	return {
+		init: function() {
+			var tabs = $('.tabs');
+			var navTabs = tabs.find('.nav-tabs')
+			var li = tabs.find('li');
+			var innerDiv = tabs.find('.chose-city-992');
+			var innerCity = innerDiv.find('.inner-city');
+
+			// navTabs.fadeOut();
+
+			innerDiv.on('click', function() {
+				navTabs.fadeToggle();
+			});
+
+			li.on('click', function() {
+				$that = $(this);
+				innerCity.text($that.text());
+			});
+		}
+	}
+})();
+
 
 $(document).ready(function() {
 	siteNavigation.init();
+	choseCity.init();
 });
